3b9ae25153c5a79be9efe076ada744a97963fff0
Chad Eymard <eymardcm@gmail.com>
2024-02-08 19:22:52 -0500

    init

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Chad Eymard on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
